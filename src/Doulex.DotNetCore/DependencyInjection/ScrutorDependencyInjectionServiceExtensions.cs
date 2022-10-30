ff002bb7f2f21a3318fceec01603017820272c79
nepton <nepton@qq.com>
2022-10-30 15:03:20 +0800

    add scrutor di

---
+++ src/Doulex.DotNetCore/DependencyInjection/ScrutorDependencyInjectionServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+
+namespace Doulex.DotNetCore;
+
+public static class ScrutorDependencyInjectionServiceExtensions
+{
+    public static IServiceCollection AddDependencyInjectionInProjects(this IServiceCollection services)
+    {
+        services.Scan(scan =>
+        {
+            var projects = DependencyContext.Default.CompileLibraries
+                .Where(lib => lib.Type == "project")
+                .Select(lib => lib.Name)
+                .ToList();
+
+            scan.FromApplicationDependencies(lib => projects.Any(p => lib.FullName?.StartsWith(p) ?? false))
+                .AddClasses()
+                .UsingAttributes();
+        });
+
+        return services;
+    }
+}
