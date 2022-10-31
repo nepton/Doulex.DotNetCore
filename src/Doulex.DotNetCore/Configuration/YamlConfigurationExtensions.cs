184ae9881c1f95f1f4c0f982ac8489cff084ff06
nepton <nepton@qq.com>
2022-10-31 09:27:20 +0800

    yaml configuration

---
+++ src/Doulex.DotNetCore/Configuration/YamlConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace Doulex.DotNetCore.Configuration;
+
+public static class YamlConfigurationExtensions
+{
+    public static void ReplaceJsonFilesToYaml(this IConfigurationBuilder builder)
+    {
+        var jsonFiles = builder.Sources.Where(s => s.GetType() == typeof(JsonConfigurationSource)).OfType<JsonConfigurationSource>().ToArray();
+        foreach (var jsonFile in jsonFiles)
+        {
+            builder.Sources.Remove(jsonFile);
+            var yamlPath = Path.ChangeExtension(jsonFile.Path, "yml");
+            builder.AddYamlFile(yamlPath, jsonFile.Optional, jsonFile.ReloadOnChange);
+        }
+    }
+}
